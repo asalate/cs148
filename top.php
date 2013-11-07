35f1ebb5ea2b7716bcaf254756cd16ac10a6c87a
Anna Marie Salate <asalate@okapi.uvm.edu>
2013-11-06 22:48:40 -0500

    5th commit

---
+++ top.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Wall of Photos</title>
+<meta charset="utf-8">
+<meta name="author" content="Anna Salate">
+<meta name="description" content="A Wall of Photos with students from UVM">
+    
+<link rel="stylesheet"
+	  href="style.css"
+      media="screen">
+
+<!--[if lt IE 9]>
+    <script src="//html5shim.googlecode.com/sin/trunk/html5.js"></script>
+<![endif]-->
+</head>
